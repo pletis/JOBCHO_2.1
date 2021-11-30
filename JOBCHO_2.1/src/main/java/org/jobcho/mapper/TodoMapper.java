4ac2ffec205a55ba0dab3edf5e36e77e21d77dc2
gunsooPark <gunsoo523@gmail.com>
2021-11-30 12:26:11 +0900

    오늘의 할일 구현 완료

--- JOBCHO_2.1/src/main/java/org/jobcho/mapper/TodoMapper.java
+++ JOBCHO_2.1/src/main/java/org/jobcho/mapper/TodoMapper.java
@@ -1,7 +1,13 @@
 package org.jobcho.mapper;
 
+import java.util.List;
+
 import org.jobcho.domain.TodoVO;
 
 public interface TodoMapper {
 	public int insertTodo(TodoVO todo);
+	public TodoVO getTodo(int todo_num);
+	public List<TodoVO> selectTodoList();
+	public int updateTodo(TodoVO todo);
+	public int deleteTodo(int todo_num);
 }
--- JOBCHO_2.1/src/main/java/org/jobcho/service/TodoService.java
+++ JOBCHO_2.1/src/main/java/org/jobcho/service/TodoService.java
@@ -1,7 +1,13 @@
 package org.jobcho.service;
 
+import java.util.List;
+
 import org.jobcho.domain.TodoVO;
 
 public interface TodoService {
 	public int insertTodo(TodoVO todo);
+	public TodoVO getTodo(int todo_num);
+	public List<TodoVO> selectTodoList();
+	public int updateTodo(TodoVO todo);
+	public int deleteTodo(int todo_num);
 }
