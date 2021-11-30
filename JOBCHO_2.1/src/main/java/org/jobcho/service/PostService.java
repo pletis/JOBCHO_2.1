69ab26883d480a299e99d4579296d518722e27cb
YooJongHyeon <justin77748292@gmail.com>
2021-11-30 14:16:05 +0900

    종현

--- JOBCHO_2.1/src/main/java/org/jobcho/service/PostService.java
+++ JOBCHO_2.1/src/main/java/org/jobcho/service/PostService.java
@@ -4,17 +4,17 @@ import java.util.List;
 
 import org.jobcho.domain.PostVO;
 
-import oracle.jdbc.proxy.annotation.Post;
+//import oracle.jdbc.proxy.annotation.Post;
 
 public interface PostService {
 	
 	
-	public int insertPost(PostVO post); //게시글 생성
+	public int insertPost(PostVO post); //寃뚯떆湲� �깮�꽦
 	
-	public List<PostVO> getListPost(int board_num); //게시글 리스트
+	public List<PostVO> getListPost(int board_num); //寃뚯떆湲� 由ъ뒪�듃
 	
-	public int updateBoard(PostVO post); //게시글 수정
+	public int updateBoard(PostVO post); //寃뚯떆湲� �닔�젙
 	
-	public void deletePost(int post_num); //게시글 삭제
+	public void deletePost(int post_num); //寃뚯떆湲� �궘�젣
 	
 }
