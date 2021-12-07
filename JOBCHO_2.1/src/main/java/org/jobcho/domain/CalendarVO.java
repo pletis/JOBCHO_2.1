e1fc71676a7eaa57f498d345e517a71c94fe05a0
YooJongHyeon <justin77748292@gmail.com>
2021-12-07 10:59:56 +0900

    날짜에러 1207

--- JOBCHO_2.1/src/main/java/org/jobcho/domain/CalendarVO.java
+++ JOBCHO_2.1/src/main/java/org/jobcho/domain/CalendarVO.java
@@ -3,13 +3,18 @@ package org.jobcho.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class CalendarVO implements Serializable{
 	private int cal_num;
 	private String cal_title;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
 	private Date starts;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
 	private Date ends;
 	private int allday;
 	private int isLive;
