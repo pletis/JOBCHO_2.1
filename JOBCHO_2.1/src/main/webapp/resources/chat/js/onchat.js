b4fd729c28748bc8667eb62d3bba80e804ca5a08
choi <boy0516@naver.com>
2021-12-09 10:25:40 +0900

    채팅방초대 후 꺼짐

--- JOBCHO_2.1/src/main/webapp/resources/chat/js/onchat.js
+++ JOBCHO_2.1/src/main/webapp/resources/chat/js/onchat.js
@@ -195,6 +195,8 @@ $(document).ready(function(){
 		        dataType:'json',
 		        success:function(result){
 		        	console.log("member invite succss");
+		        	$("#insertChatRoomModal").modal("hide");
+		        	alert("채팅방 생성");
 		        }
 	    	});
 		})
